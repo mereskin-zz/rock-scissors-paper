29bd520276258f3cfad1ace46879750dedf96019
Andrey Mereskin <mereskin@gmail.com>
2014-06-11 20:36:15 +0400

    Create test for /play

--- test/server-spec.js
+++ test/server-spec.js
@@ -5,11 +5,17 @@ var request = require('request');
 describe('Rock scissors paper server', function(){
 
   it('GET / should return 200', function(done){
-
     request.get('http://localhost:8080', function(err, res, body){
       assert.equal(res.statusCode, 200);
       done();
     });
-
   });
+
+  it('POST /play should return 200', function(done){
+    request.post('http://localhost:8080/play', {form:{choice:2}}, function(err, res, body){
+      assert.equal(res.statusCode, 200);
+      done();
+    });
+  });
+
 });
