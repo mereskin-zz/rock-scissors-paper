e95b57e2a79cd4a2b3201329b7798b5c047f3329
Andrey Mereskin <mereskin@gmail.com>
2014-06-11 21:33:39 +0400

    /play endpoint working

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var game = require('../src/game');
+
 var app = express();
+
+app.use(bodyParser());
 
 app.get('/', function(req, res){
   res.send('hello world');
+});
+
+app.all('/play', function(req, res){
+  var userChoice = req.param('choice');
+  var computerChoice = game.getComputerChoice();
+  var result = game.getWinner(userChoice, computerChoice);
+
+  res.json({
+
+    userChoice: userChoice,
+    computerChoice: computerChoice,
+    result: result,
+
+    computerChoiceName: game.CHOICES[computerChoice],
+    winnerName: game.WINNERS[result]
+
+  });
 });
 
 app.listen(8080);
--- test/server-spec.js
+++ test/server-spec.js
@@ -18,4 +18,14 @@ describe('Rock scissors paper server', function(){
     });
   });
 
+  it('POST /play should return json with result', function(done){
+    request.post('http://localhost:8080/play', { form:{ choice: 2 } }, function(err, res, body){
+      var result = JSON.parse(body);
+
+      assert(result.computerChoice >= 0);
+      assert(result.result >= 0);
+
+      done();
+    });
+  });
 });
