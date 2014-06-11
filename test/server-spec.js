18bede3d8080d49e841e0f381068b68aa71cb027
Andrey Mereskin <mereskin@gmail.com>
2014-06-11 20:25:05 +0400

    Make test pass

--- test/server-spec.js
+++ test/server-spec.js
@@ -2,13 +2,11 @@ var server = require('../src/server');
 var assert = require('assert');
 var request = require('request');
 
-server.listen(8080);
-
 describe('Rock scissors paper server', function(){
 
   it('GET / should return 200', function(done){
 
-    request.get('http://localhost:8080', function(err, res){
+    request.get('http://localhost:8080', function(err, res, body){
       assert.equal(res.statusCode, 200);
       done();
     });
